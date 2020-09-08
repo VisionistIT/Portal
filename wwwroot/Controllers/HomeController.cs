79d0929b64d27ded61681c246c0b74cdca9ae7f1
saharmerheb <merheb_sahar@hotmail.com>
2020-09-08 16:48:36 +0300

    init

---
+++ wwwroot/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Visionist.Portal.wwwroot.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(string language="en")
+        {
+            string content = System.IO.File.ReadAllText("./wwwroot/Index.html");
+            return base.Content(content, new MediaTypeHeaderValue("text/html"));
+
+        }
+    }
+}
---
+++ wwwroot/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Visionist.Portal.wwwroot.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost]
+        public IActionResult Send([FromBody] models.Message message)
+        {            
+            var sessionInfo = JsonConvert.SerializeObject(Request.Headers);
+            //Data.Portal.SaveMessage(sessionInfo, message.name, message.email, message.phone, message.message);
+            EmailHelper.Send(message.name, message.email, message.phone, message.message);
+            return Ok();
+        }
+    }
+}
